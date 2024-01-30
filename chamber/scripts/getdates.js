c054dcdcc52dc00e1cc3787f220ed77644f3a919
trenton1231 <71413044+trenton1231@users.noreply.github.com>
2024-01-30 23:31:13 +0000

    Week 4 items

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,9 @@
+let d = new Date();
+let year = d.getFullYear();
+let fullYear = year;
+
+document.getElementById("getYear").textContent = fullYear;
+
+document.getElementById('lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
+
+
