b19aa50c381bc400151e2dae82d59b9333d1df5e
trenton1231 <71413044+trenton1231@users.noreply.github.com>
2024-03-01 01:02:42 +0000

    Lesson 7 and 8 Chamber pages

---
+++ chamber/scripts/form.js
@@ -0,0 +1,20 @@
+const kp1 = document.querySelector("#keyphrase");
+const kp2 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+    if (kp1.value !== kp2.value) {
+        message.textContent = "❗Key Phrases DO NOT MATCH!";
+        message.style.visibility = "show";
+        kp2.style.backgroundColor = "#fff0f3";
+        kp2.value = "";
+        kp2.focus();
+    } else {
+        message.style.display = "none";
+        kp2.style.backgroundColor = "#fff";
+        kp2.style.color = "#000";
+    }
+}
