caff91629df7d70f07c8565476636dca9738c694
trenton1231 <71413044+trenton1231@users.noreply.github.com>
2024-03-07 23:51:58 +0000

    Collab week 9

---
+++ scripts/links.js
@@ -0,0 +1,34 @@
+const baseURL = "https://trenton1231.github.io/wd230/";
+const lists = document.querySelector('#learningActivities');
+const linksURL = "https://trenton1231.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch("https://trenton1231.github.io/wdd230/data/links.json");
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+
+getLinks();
+
+const displayLinks = (weeks) => {
+    weeks.forEach((lesson) => {
+        const list = document.createElement('li');
+        list.textContent = 'Week ${lesson.lesson: ';
+        lesson.links.forEach(function (week, index) {
+            const link = document.createElement('a');
+            link.textContent = '${week.title}';
+            link.setAttribute('href', '${week.url}')
+            link.setAttribute('target', '_blank')
+
+            list.appendChild(link);
+
+            if (index < lesson.links.length - 1 && list.childElementCount > 0) {
+                var space = document.createTextNode('|');
+                list.appendChild(space);
+            }
+
+        });
+        lists.appendChild(list);
+
+    });
+}
