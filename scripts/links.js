bb2e212f391911d0245efec47a89a006db093087
trenton1231 <71413044+trenton1231@users.noreply.github.com>
2024-03-08 00:08:35 +0000

    trying again

--- scripts/links.js
+++ scripts/links.js
@@ -1,24 +1,32 @@
-const baseURL = "https://trenton1231.github.io/wd230/";
+const baseURL = "https://trenton1231.github.io/wdd230/";
+
 const lists = document.querySelector('#learningActivities');
+
 const linksURL = "https://trenton1231.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch("https://trenton1231.github.io/wdd230/data/links.json");
     const data = await response.json();
     displayLinks(data.lessons);
+
 }
 
 getLinks();
 
 const displayLinks = (weeks) => {
     weeks.forEach((lesson) => {
+
         const list = document.createElement('li');
-        list.textContent = 'Week ${lesson.lesson: ';
+
+        list.textContent = `Week ${lesson.lesson}: `;
+
         lesson.links.forEach(function (week, index) {
+
             const link = document.createElement('a');
-            link.textContent = '${week.title}';
-            link.setAttribute('href', '${week.url}')
-            link.setAttribute('target', '_blank')
+
+            link.textContent = `${week.title}`;
+            link.setAttribute('href', `${week.url}`)
+            link.setAttribute('target', `_blank`)
 
             list.appendChild(link);
 
@@ -27,7 +35,10 @@ const displayLinks = (weeks) => {
                 list.appendChild(space);
             }
 
+
         });
+
+
         lists.appendChild(list);
 
     });
